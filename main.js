c8754793bdb48140e27478c85b7c0522c3d69712
killeraliens <killeraliens@outlook.com>
2019-06-26 23:45:01 -0700

    handle night switch

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-$("button").click(function(e) {
-  // console.log($(this).attr('href'));
-})
+
+
+function handleThemeSwitch() {
+    $("#js-color-theme-switch").on('click', function(e) {
+        $("body").toggleClass('night');
+        const switchInput = $(e.currentTarget);
+        switchInput.attr("checked", !switchInput.attr("checked"));
+        switchInput.prop("aria-checked", !switchInput.prop("aria-checked"));
+
+        console.log(switchInput.prop('aria-checked'));
+        console.log(switchInput.prop('checked'));
+    });
+}
+
+
+
+
+
+
+
+
+function handleInputs() {
+    handleThemeSwitch();
+
+}
+
+handleInputs();
