fca3f0f37166bd4e3db03256780ce8e56f3b3ec6
killeraliens <killeraliens@outlook.com>
2019-06-28 17:22:54 -0700

    free formspree added

--- components/contact.js
+++ components/contact.js
@@ -1,15 +1,15 @@
+// FOR USE WITH MANDRILL $$$
+// function Email(name, email, message) {
+//     this.senderName = name;
+//     this.senderEmail = email;
+//     this.message = message;
+//     this.subject = `${this.senderName} sent you a message from your site`;
+//     this.recipient = `alexandrabrinncampbell@gmail.com`;
+//     this.recipientName = `Alexandra Campbell`;
+// }
 
-function Email(name, email, message) {
-    this.senderName = name;
-    this.senderEmail = email;
-    this.message = message;
-    this.subject = `${this.senderName} sent you a message from your site`;
-    this.recipient = `alexandrabrinncampbell@gmail.com`;
-    this.recipientName = `Alexandra Campbell`;
-}
-
-Email.prototype.send = function() {
-    console.log('email sent');
+// Email.prototype.send = function() {
+//     console.log('email sent');
 //     $.ajax({
 //         type: “POST”,
 //     url: “https://mandrillapp.com/api/1.0/messages/send.json”,
@@ -32,39 +32,50 @@ Email.prototype.send = function() {
 // }).done(function(response) {
 //         console.log(response); // if you're into that sorta thing
 //     });
-}
+// }
 
-function handleEmailFormSubmit() {
-    $("#js-contact-form").on('submit', function(e) {
-        e.preventDefault();
-        const senderName = $(e.currentTarget).find("#name").val();
-        const senderEmail = $(e.currentTarget).find("#email").val();
-        const message = $(e.currentTarget).find("#message").val();
-        newEmail = new Email(senderName, senderEmail, message);
-        console.log(newEmail.senderName + newEmail.senderEmail + newEmail.message);
-        newEmail.send();
-    })
-}
+// function handleEmailFormSubmit() {
+//     $("#js-contact-form").on('submit', function(e) {
+//         e.preventDefault();
+//         const senderName = $(e.currentTarget).find("#name").val();
+//         const senderEmail = $(e.currentTarget).find("#email").val();
+//         const message = $(e.currentTarget).find("#message").val();
+//         newEmail = new Email(senderName, senderEmail, message);
+//         console.log(newEmail.senderName + newEmail.senderEmail + newEmail.message);
+//         newEmail.send();
+//     })
+// }
+
 
 
 function returnContactSectionHtml() {
   return `
     <section id="contactSection" class="container vertical-center-container">
       <div class="vertical-center-content form-wrap">
-          <form id="js-contact-form" class="contact-form" action="" method="post" enctype="text/plain">
+          <form id="js-contact-form" class="contact-form" action="https://formspree.io/alexandrabrinncampbell@gmail.com" method="POST" >
             <h1>Get In Touch</h1>
             <fieldset name="contact-info">
               <label for="name" required>Name</label>
               <input id="name" name="name" type="text" placeholder="Your Name" required>
               <label for="email" required>Email</label>
-              <input id="email" name="email" type="email" placeholder="Your Email" required>
+              <input id="email" name="_replyto" type="email" placeholder="Your Email" required>
             </fieldset>
             <fieldset name="message-content">
               <label for="message" required>Message</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </fieldset>
-            <button id="js-submit-email-button" type="submit">Send</button>
+            <input type="hidden" name="_format" value="plain">
+            <input type="hidden" name="_next" value="https://killeraliens.github.io/alexandra-brinn/index.html" />
+            <button id="js-submit-email-button" type="submit" >Send</button>
           </form>
+<!--        <form action="https://formspree.io/alexandrabrinncampbell@gmail.com" method="POST">-->
+<!--            <input type="text" name="name">-->
+<!--            <input type="email" name="_replyto">-->
+<!--            <textarea type="text" name="description" id="message" rows="4" cols="10"></textarea>-->
+<!--            -->
+<!--            <input type="text" name="_gotcha" style="display:none" />-->
+<!--            <input type="submit" value="Send">-->
+<!--        </form>-->
       </div>
     </section>
   `;
