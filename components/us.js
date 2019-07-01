beb6374965c2c9c367ea3a23b5cc90103ee29038
killeraliens <killeraliens@outlook.com>
2019-07-01 09:44:07 -0700

    question option html

--- components/us.js
+++ components/us.js
@@ -5,7 +5,12 @@ function returnUsSectionString() {
               <p><b>Fill out this form to test our compatibility. The results are only visible to you.</b></p>
               <form id="js-us-form">
                 <fieldset>
-                  <legend></legend>
+                  <legend class="question">How might your dieting practices best be described?</legend>
+                  <label>
+                    <img class="us-option-img" src="../assets/cryptids-quiz/mobile-1.png" alt="qOptionImg">
+                    <input type="radio" id="radio1" name="option" value="qOptionA">
+                    <span>qOptionA</span>
+                  </label>
                 </fieldset>
               </form>
           </div>
