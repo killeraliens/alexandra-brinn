271c8eb7a6fbc8fd056edb1e4a0c9b5df2ca77d8
killeraliens <killeraliens@outlook.com>
2020-03-11 09:48:55 -0700

    about copy

--- components/about.js
+++ components/about.js
@@ -5,15 +5,15 @@ function returnAboutSectionString() {
               <h1>Hi, I'm Alexandra - AKA Ali, and I love designing and scripting digital experiences for people.</h1>
               <p>
                 I enjoy all aspects of web development from beginning-to-end. This includes user-flow illustration, HTML/CSS wireframing, database planning,
-                and implementation of server-side languages like JavaScript, Ruby & SQL. Most of all, I enjoy learning
-                and experimenting with new software and languages which allow me to express, implement, and test creative concepts.
+                and implementation of server-side languages like Node.js, Ruby & SQL. Most of all, I enjoy learning
+                and experimenting with new libraries and languages which allow me to express, implement, and test creative concepts.
               </p>
               <p>
                 And when I'm not traveling, prototyping personal projects, or blasting my favorite LPs,
                 you can find me pursuing more in-depth knowledge on JavaScript,
-                React, and Node within Thinkful's full-stack program.
+                React, and Node with my personal project <a href="https://goatsguide.com" target="_blank">goatsguide.com</a>.
               </p>
-              <p><b>I am currently looking for a fun and organized team with experienced developers
+              <p><b>I am currently looking for a team of experienced developers and creatives
                 where I can contribute by combining my new coding skills with UX design. Contact me regarding potential projects.</b>
               </p>
           </div>
