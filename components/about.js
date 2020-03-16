d8ab280be69413fe76501e5ba9c85e59ce3465b1
killeraliens <killeraliens@outlook.com>
2020-03-16 09:52:57 -0700

    about copy

--- components/about.js
+++ components/about.js
@@ -1,20 +1,20 @@
 function returnAboutSectionString() {
     return `
          <section id="aboutSection" class="container">
-          <div class="v">
+          <div class="">
               <h1>Hi, I'm Alexandra - AKA Ali, and I love designing and scripting digital experiences for people.</h1>
               <p>
-                I enjoy all aspects of web development from beginning-to-end. This includes user-flow illustration, HTML/CSS wireframing, database planning,
-                and implementation of server-side languages like Node.js, Ruby & SQL. Most of all, I enjoy learning
-                and experimenting with new libraries and languages which allow me to express, implement, and test creative concepts.
+                This includes user-flow illustration, HTML/CSS wireframing, and REST api development using React, Node.js and SQL.
+                Most of all, I enjoy learning and experimenting with new libraries and languages which allow me to express, implement, and test creative concepts.
               </p>
               <p>
-                And when I'm not traveling, prototyping personal projects, or blasting my favorite LPs,
-                you can find me pursuing more in-depth knowledge on JavaScript,
-                React, and Node with my personal project <a href="https://goatsguide.com" target="_blank">goatsguide.com</a>.
+                And when I'm not traveling, coming up with new designs, or blasting my favorite LPs,
+                you can find me honing my skills in React and Node.js with my personal project
+                <a href="https://goatsguide.com" target="_blank">goatsguide.com</a>,
+                a site dedicated to the collection of heavy metal event artwork and related data.
               </p>
-              <p><b>I am currently looking for a team of experienced developers and creatives
-                where I can contribute by combining my new coding skills with UX design. Contact me regarding potential projects.</b>
+              <p><b>I am currently looking to join a team of experienced developers
+                where I can contribute by combining my coding skills with UX design. Contact me regarding potential projects.</b>
               </p>
           </div>
         </section>
