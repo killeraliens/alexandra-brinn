754bf5041ca739bb4cf169c15c94cb8e87f597a8
killeraliens <killeraliens@outlook.com>
2020-04-24 18:24:06 -0700

    Js typo fix

--- components/about.js
+++ components/about.js
@@ -4,17 +4,17 @@ function returnAboutSectionString() {
           <div class="">
               <h1>Hi, I'm Alexandra - AKA Ali, and I create interactive digital experiences.</h1>
               <p>
-                This includes user-flow illustration, HTML/CSS/JavaScript wireframing, React SPAs, and REST API development using NodeJs, Express, and PostgreSQL.
+                This includes user-flow illustration, HTML/CSS/JavaScript wireframing, React SPAs, and REST API development using NodeJS, Express, and PostgreSQL.
               </p>
               <p>
                 I earned my BA in industrial design from Emily Carr University Vancouver, BC in 2007,
                 and have since made my way towards creativity through code.
                 In early 2019 taking a nine week course with Le Wagon (London) led me to Ruby on Rails and collaborative app development.
                 I then continued on with Thinkful’s software engineering course, learning the PERN stack through early 2020,
-                where I was able to conquer my fear of JavaScript and build my first TDD API using NodeJs and Express.
+                where I was able to conquer my fear of JavaScript and build my first TDD API using NodeJS and Express.
               </p>
               <p>
-                Recently, I’ve been sharpening my skills in React and NodeJs with my personal project
+                Recently, I’ve been sharpening my skills in React and NodeJS with my personal project
                 <a href="https://goatsguide.com" target="_blank">goatsguide.com</a>,
                 a site dedicated to the collection of concert flier artwork and event-related data.
               </p>
